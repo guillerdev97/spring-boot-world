ec8c2e430de5362b3a8c2ccebef245f1e6a799a5
Guillermo García <guillermieres97@gmail.com>
2022-10-30 11:10:38 +0100

    test get method hellocontroller

--- src/main/java/com/guillerdev97/SpringBootCourse/controller/HelloController.java
+++ src/main/java/com/guillerdev97/SpringBootCourse/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.guillerdev97.SpringBootCourse.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome() {
+        return "Hello my friends";
+    }
 }
