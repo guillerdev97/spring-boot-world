20fc0020e4fce0eccbd5594882fa017d7eed0711
Guillermo García <guillermieres97@gmail.com>
2022-10-30 12:36:46 +0100

    add department repository

---
+++ src/main/java/com/guillerdev97/SpringBootCourse/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.guillerdev97.SpringBootCourse.repository;
+
+import com.guillerdev97.SpringBootCourse.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
