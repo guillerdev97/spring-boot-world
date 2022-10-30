6eb3c2e920c20196449d96d979753bb14b3b2b7a
Guillermo García <guillermieres97@gmail.com>
2022-10-30 11:01:07 +0100

    initial commit

---
+++ src/test/java/com/guillerdev97/SpringBootCourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillerdev97.SpringBootCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
