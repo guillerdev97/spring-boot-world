fb30fc441e567aff5120eb695ec1ed725cfa4aa8
Guillermo García <guillermieres97@gmail.com>
2022-11-01 08:37:14 +0100

    feat: create test fetch department by name

--- src/main/java/com/guillerdev97/SpringBootCourse/entity/Department.java
+++ src/main/java/com/guillerdev97/SpringBootCourse/entity/Department.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
