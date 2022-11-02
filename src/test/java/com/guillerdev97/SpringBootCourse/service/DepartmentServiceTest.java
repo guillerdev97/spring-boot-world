e7a66db12410985bf0b3f905756e227d5762cc9f
Guillermo García <guillermieres97@gmail.com>
2022-11-02 10:20:10 +0100

    test with mockito

--- src/test/java/com/guillerdev97/SpringBootCourse/service/DepartmentServiceTest.java
+++ src/test/java/com/guillerdev97/SpringBootCourse/service/DepartmentServiceTest.java
@@ -1,28 +1,42 @@
 package com.guillerdev97.SpringBootCourse.service;
 
 import com.guillerdev97.SpringBootCourse.entity.Department;
+import com.guillerdev97.SpringBootCourse.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DepartmentServiceTest {
     @Autowired
     private DepartmentService departmentService;
 
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
     @BeforeEach
     void setUp() {
+        Department department = Department.builder()
+                .departmentName("SAS")
+                .departmentAddress("Gijón")
+                .departmentCode("111")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("SAS"))
+                .thenReturn(department);
     }
 
-    public void if_department_list_is_found
 
     @Test
-    public void givenDepartmentName_thenDepartmentFound() {
+    public void if_given_department_name_department_found() {
         // given
-        String departmentName = "A-123";
+        String departmentName = "SAS";
 
         // when
         Department departmentFound = departmentService.fetchDepartmentByName(departmentName);
