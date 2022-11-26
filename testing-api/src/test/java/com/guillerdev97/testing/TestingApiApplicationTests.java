c5934d354dc3fbf593bf748086e3f20e789a2fa6
Guillermo García <guillermieres97@gmail.com>
2022-11-26 12:08:45 +0100

    new testing project

---
+++ testing-api/src/test/java/com/guillerdev97/testing/TestingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillerdev97.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
